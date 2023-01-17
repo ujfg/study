edbe2f0b3cdf4a1bf5f64a11cd8f25fcbae52b76
ujfg <ujeong.fg@gmail.com>
2023-01-17 12:22:05 +0900

    section 11 終わりまで

--- te_ugokasu_type_script/src/index.ts
+++ te_ugokasu_type_script/src/index.ts
@@ -1,5 +1,20 @@
-const sayHello = (name: string) => {
-  return `Hello, ${name}!`
+const printLine = (text: string, breakLine: boolean = true) => {
+  process.stdout.write(text + (breakLine ? '\n' : ''))
 }
 
-process.stdout.write(sayHello('John'))
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n`, false)
+  const input: string = await new Promise(
+    (resolve) => process.stdin.once('data', (data) => resolve(data.toString()))
+  )
+  return input.trim()
+}
+
+// 即時関数で囲まないと、prompyInputがpromiseインスタンスを返して処理が次に進んでしまう
+;(async () => {
+  const name = await promptInput('名前を入力してください')
+  console.log(name)
+  const age = await promptInput('年齢を入力してください')
+  console.log(age)
+  process.exit()
+})()
